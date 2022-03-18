92d446bba684026c3dafec20df889b6a5181fe92
Codrin Iftimie <iceboxic@gmail.com>
2022-03-18 17:42:18 +0200

    Fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+// import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only'
 import pkg from './package.json';
@@ -10,8 +10,8 @@ export default [
 		input: 'src/index.ts',
 		external: ['antd', 'react'],
         plugins: [
-			resolve(), // so Rollup can find `ms`
-			commonjs(), // so Rollup can convert `ms` to an ES module
+			// resolve(), // so Rollup can find `ms`
+			// commonjs(), // so Rollup can convert `ms` to an ES module
 			typescript(),
 			css({ output: 'styles.css' })
 		],
